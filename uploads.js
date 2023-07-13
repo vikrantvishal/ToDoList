839bed2faab8ed4f52a31432d79820d7f83afd85
Tanmay <121565463+tanmay2809@users.noreply.github.com>
2023-07-13 22:55:50 +0530

    Update uploads.js

--- uploads.js
+++ uploads.js
@@ -69,4 +69,428 @@ ize={30}   className="text-deep"/>
                     </div>
                     
                   </div>
+
+ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+
+ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+
+ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    ize={30}   className="text-deep"/>
+                  </div>
+                  <div className="flex flex-col">
+                    <h4 className=" font-bold">Languages</h4>
+                    <div className="max-w-[18rem]"> 
+                      <p>Languages that I have picked up over the years</p>
+                     
+                    </div>
+                    
+                  </div>
+                    
                     
